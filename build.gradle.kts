db85de478fa8809ce5263d0dcebdb0cea8e74987
Malibin <nightmare73@naver.com>
2020-10-06 21:48:00 +0900

    [Initial] 초기 설정 세팅

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.10"
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+}
+
+group = "study.malibin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
+    testImplementation("org.assertj", "assertj-core", "3.16.1")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+    ktlint {
+        verbose.set(true)
+        disabledRules.addAll("import-ordering")
+    }
+}
