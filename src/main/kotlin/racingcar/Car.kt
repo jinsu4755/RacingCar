90ab79cb235d03735fceac2c02f81ee3fce5874b
RoadtoS7 <kshvv975@gmail.com>
2020-11-01 22:57:17 +0900

    feat(Car.kt): add Car.kt
    자동차 하나에 대한 클래스를 만들었다

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,17 @@
+package racingcar
+
+class Car {
+    private var distanceGraph: String = ""
+
+    fun run() {
+        if(RaceRule.checkIfCarRun()){
+            distanceGraph += ONE_DISTANCE
+        }
+    }
+
+    fun getDistanceGraph() = distanceGraph
+
+    companion object {
+        const val ONE_DISTANCE = "-"
+    }
+}
