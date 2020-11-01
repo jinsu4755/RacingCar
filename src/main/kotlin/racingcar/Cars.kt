12738567c98c590fd992218afeed651a79f441c5
RoadtoS7 <kshvv975@gmail.com>
2020-11-01 22:59:22 +0900

    feat(Cars.kt): add Cars.kt
    자동차 여러대를 다루는 클래스를 만들었다

---
+++ src/main/kotlin/racingcar/Cars.kt
@@ -0,0 +1,31 @@
+package racingcar
+
+class Cars(carNum: Int) {
+    private val carList = mutableListOf<Car>()
+
+    init {
+        createCar(carNum)
+    }
+
+    private fun createCar(carNum: Int) {
+        for (i in 0 until carNum) {
+            carList.add(Car())
+        }
+    }
+
+    fun run(carIndex: Int) {
+        carList[carIndex].run()
+    }
+
+    fun getDistanceGraph(): String {
+        var distanceGraph = ""
+        carList.forEach {car ->
+            distanceGraph = distanceGraph + car.getDistanceGraph() + NEW_LINE
+        }
+        return distanceGraph
+    }
+
+    companion object {
+        private const val NEW_LINE = "\n"
+    }
+}
