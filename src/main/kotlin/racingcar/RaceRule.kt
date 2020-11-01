94a659400ee2f6a0d0c9c7da0759dd873ffda3c1
RoadtoS7 <kshvv975@gmail.com>
2020-11-01 23:02:17 +0900

    feat(RaceRule.kt): add RaceRule class for check if car run
    차가 달릴 수 있는 규칙에 따라서 랜덤넘버를 생성하고 체크하는 함수를 가지는 클래스를 만들었다

---
+++ src/main/kotlin/racingcar/RaceRule.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+object RaceRule{
+    private val randomRange = IntRange(0, 9)
+    private const val ruleNumber = 4
+
+    fun checkIfCarRun(): Boolean {
+        val randomNum = randomRange.random()
+        return randomNum >= ruleNumber
+    }
+}
