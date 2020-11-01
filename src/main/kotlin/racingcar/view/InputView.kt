d528e62986176ee17f7be9fac0279fade7c0f7e6
RoadtoS7 <kshvv975@gmail.com>
2020-11-01 23:07:01 +0900

    feat(InputView.kt): add InputView
    사용자로부터 입력을 받기 위해 사용하는 함수를 담은 InputView를 만들었다

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,17 @@
+package racingcar.view
+
+import java.lang.IllegalArgumentException
+
+object InputView {
+    fun inputCarNum(): Int {
+        println("자동차 대수는 몇 대인가요?")
+        return readLine()?.toInt()
+            ?: throw IllegalArgumentException("자동차 대수는 반드시 입력해야 합니다.")
+    }
+
+    fun inputRaceNum(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readLine()?.toInt()
+            ?: throw IllegalArgumentException("시도할 횟수는 반드시 입력해야 합니다.")
+    }
+}
