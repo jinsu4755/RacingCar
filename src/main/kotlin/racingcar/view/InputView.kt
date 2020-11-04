a3601dbe36955770f3eefec96e716aec8467cfce
RoadtoS7 <kshvv975@gmail.com>
2020-11-04 09:54:10 +0900

    refactor(InputView.kt): Fix functions name

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -3,13 +3,13 @@ package racingcar.view
 import java.lang.IllegalArgumentException
 
 object InputView {
-    fun inputCarNum(): Int {
+    fun getCarNum(): Int {
         println("자동차 대수는 몇 대인가요?")
         return readLine()?.toInt()
             ?: throw IllegalArgumentException("자동차 대수는 반드시 입력해야 합니다.")
     }
 
-    fun inputRaceNum(): Int {
+    fun getRaceNum(): Int {
         println("시도할 횟수는 몇 회인가요?")
         return readLine()?.toInt()
             ?: throw IllegalArgumentException("시도할 횟수는 반드시 입력해야 합니다.")
