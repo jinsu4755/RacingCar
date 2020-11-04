0bc11064b14312dedd29a4c7762ed33557a24404
RoadtoS7 <kshvv975@gmail.com>
2020-11-04 10:13:08 +0900

    refactor(RaceRule.kt): fix constant name so that it satisfies variable naming convention

--- src/main/kotlin/racingcar/RaceRule.kt
+++ src/main/kotlin/racingcar/RaceRule.kt
@@ -2,10 +2,10 @@ package racingcar
 
 object RaceRule {
     private val randomRange = IntRange(0, 9)
-    private const val ruleNumber = 4
+    private const val RULE_NUMBER = 4
 
     fun checkIfCarRun(): Boolean {
         val randomNum = randomRange.random()
-        return randomNum >= ruleNumber
+        return randomNum >= RULE_NUMBER
     }
 }
