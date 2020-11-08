1a0d2c2678b512bc90c0b9768968aac2979cc81a
RoadtoS7 <kshvv975@gmail.com>
2020-11-08 23:51:09 +0900

    test(CarsTest.kt): test code of add() and run()

--- src/test/kotlin/racingcar/CarsTest.kt
+++ src/test/kotlin/racingcar/CarsTest.kt
@@ -1,34 +1,53 @@
-// package racingcar
-//
-// import org.assertj.core.api.Assertions.assertThat
-// import org.junit.jupiter.api.Test
-//
-// class CarsTest {
-//     @Test
-//     fun `자동차들의 달리기 함수 체크`() {
-//         // given
-//         val cars = Cars(3)
-//         // when
-//         for (i in 0 until 3) {
-//             cars.run(i)
-//         }
-//         val result = cars.getDistanceGraph()
-//         // then
-//         assertThat(result)
-//             .isEqualTo("-\n-\n-\n")
-//     }
-//
-//     @Test
-//     fun `자동차들의 현재 기록 반환함수`() {
-//         // given
-//         val cars = Cars(3)
-//         // when
-//         for (i in 0 until 3) {
-//             cars.run(i)
-//         }
-//         val result = cars.getDistanceGraph()
-//         // then
-//         assertThat(result)
-//             .isEqualTo("-\n-\n-\n")
-//     }
-// }
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class CarsTest {
+    @ParameterizedTest
+    @CsvSource("3")
+    fun `자동차 추가하는 함수 체크`(carNum: Int) {
+        // given
+        val cars = Cars()
+
+        // when
+        repeat(carNum) {
+            val car = Car(object : Checker {
+                override fun checkIfRun() = true
+            })
+            cars.add(car)
+        }
+
+        // then
+        assertThat(
+            cars.get()
+                .size
+        ).isEqualTo(carNum)
+    }
+
+    @ParameterizedTest
+    @CsvSource("5, 7")
+    fun `자동차들의 달리기 함수 체크`(carNum: Int, runTime: Int) {
+        // given
+        val cars = Cars()
+
+        // when
+        repeat(carNum) {
+            val car = Car(object : Checker {
+                override fun checkIfRun() = true
+            })
+            car.run(runTime)
+            cars.add(car)
+        }
+
+        val carsList = cars.get()
+
+        // then
+
+        carsList.forEach { car ->
+            assertThat(car.distance)
+                .isEqualTo(7)
+        }
+    }
+}
