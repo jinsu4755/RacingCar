4b3007a2ec6cd2816b7af10be66e389082926e53
RoadtoS7 <kshvv975@gmail.com>
2020-11-08 22:52:27 +0900

    refactor(InputView.kt): add getCarNames() to receive names of race cars

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,11 +1,22 @@
 package racingcar.view
 
+import java.lang.IllegalArgumentException
+
 object InputView {
     fun getCarNum(): Int {
         println("자동차 대수는 몇 대인가요?")
         val carNum = readLine()?.toIntOrNull()
             ?: throw NumberFormatException("숫자만 입력이 가능합니다.")
         return carNum
+    }
+
+    fun getCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        val carNames = readLine()!!.split(',')
+        require(carNames.isNotEmpty()) {
+            throw IllegalArgumentException("자동차를 반드시 입력해야 합니다.")
+        }
+        return carNames
     }
 
     fun getRaceNum(): Int {
