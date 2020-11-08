30db93410f12e5d1370e0cf4f9534cbd0c8dbdc4
RoadtoS7 <kshvv975@gmail.com>
2020-11-08 22:48:42 +0900

    refactor(Car.kt): add nameProperty and parameter constructor
    이름을 받는 파라미터 생성자를 만들고, 차이름을 멤버 변수로 갖도록 만들었다

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,9 +1,14 @@
 package racingcar
 
+import java.lang.IllegalArgumentException
+
 class Car(
     private val carChecker: Checker
 ) {
-    private var distance = 0
+    var distance = 0
+        private set
+    var name = ""
+        private set
 
     fun run() {
         if (carChecker.checkIfRun()) {
@@ -17,7 +22,12 @@ class Car(
         }
     }
 
-    fun getDistance() = distance
+    constructor(carName: String, carChecker: Checker) : this(carChecker) {
+        require(carName.length <= 5) {
+            throw IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다. 잘못된 자동차 이름: $carName")
+        }
+        this.name = carName
+    }
 
     companion object {
         const val ONE_DISTANCE = 1
