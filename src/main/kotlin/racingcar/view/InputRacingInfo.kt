82c384a7318bb32531ca35d34e73a3a3f3d641d0
jinsu4755 <parkjinsu4755@gmail.com>
2020-11-09 00:01:11 +0900

    Feat(step3): 3차과제

--- src/main/kotlin/racingcar/view/InputRacingInfo.kt
+++ src/main/kotlin/racingcar/view/InputRacingInfo.kt
@@ -3,22 +3,17 @@ package racingcar.view
 class InputRacingInfo {
     fun getNumberOfAttempt(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        val attemptString: String = readInputDataOrIllegalArgument()
+        val attemptString: String = readLine()!!
         return attemptString.also {
             validateNumber(it)
         }.toInt()
     }
 
-    fun getNumberOfRacingCar(): Int {
-        println("자동차 대수는 몇 대인가요?")
-        val racingCarNumberString: String = readInputDataOrIllegalArgument()
-        return racingCarNumberString.also {
-            validateNumber(it)
-        }.toInt()
+    fun getCarNames(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분합니다.")
+        val racingCarNames: String = readLine()!!
+        return racingCarNames.split(",")
     }
-
-    private fun readInputDataOrIllegalArgument() =
-        readLine() ?: throw IllegalArgumentException("입력받은 값이 Null 일 수 없습니다.")
 
     fun validateNumber(numberString: String) {
         require(numberString.toIntOrNull() is Int) {
